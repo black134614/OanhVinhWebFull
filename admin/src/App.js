5a69f9579469ad42019f837dc27844ef61b000b9
Vo Minh Luan <61099649+black134614@users.noreply.github.com>
2022-04-29 15:33:59 +0700

    admin_reactjs layout components with antd
    layout react componets with antd and router dom to page

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import Tables from "./pages/Tables";
+import Billing from "./pages/Billing";
+import Rtl from "./pages/Rtl";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Main from "./components/layout/Main";
+import "antd/dist/antd.css";
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="/sign-in" exact component={SignIn} />
+        <Main>
+          <Route exact path="/dashboard" component={Home} />
+          <Route exact path="/tables" component={Tables} />
+          <Route exact path="/billing" component={Billing} />
+          <Route exact path="/rtl" component={Rtl} />
+          <Route exact path="/profile" component={Profile} />
+          <Redirect from="*" to="/dashboard" />
+        </Main>
+      </Switch>
     </div>
   );
 }
