47a567cf8e9bc20167d7c4bf48d3e42b6d070206
Vo Minh Luan <61099649+black134614@users.noreply.github.com>
2022-05-06 23:46:27 +0700

    update mvc

--- OanhVinhWeb/Controllers/HomeController.cs
+++ OanhVinhWeb/Controllers/HomeController.cs
@@ -6,27 +6,28 @@ namespace WebOanhVinh.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [Route("")]
+        [Route("trang-chu")]
+        [Route("home")]
+        [Route("home/index")]
         public IActionResult Index()
         {
+            ViewData["Title"] = "Trang Chủ | TauThuyenViet";
             return View();
         }
-
-        public IActionResult Privacy()
+        [Route("lien-he")]
+        [Route("contact")]
+        public IActionResult Contact()
         {
+            ViewData["Title"] = "Liên Hệ | TauThuyenViet";
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [Route("gioi-thieu")]
+        [Route("about")]
+        public IActionResult About()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewData["Title"] = "Giới Thiệu | TauThuyenViet";
+            return View();
         }
     }
 }
