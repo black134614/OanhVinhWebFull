78f506bf02c1e6399e1e4db7d053ffddb9a59d23
Vo Minh Luan <61099649+black134614@users.noreply.github.com>
2022-05-13 13:11:54 +0700

    chia ViewComponent

--- OanhVinhWeb/Controllers/PostController.cs
+++ OanhVinhWeb/Controllers/PostController.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
 
 namespace WebOanhVinh.Controllers
 {
     public class PostController : Controller
     {
+        public PostController()
+        {
+        }
+
         [Route("bai-dang")]
         public IActionResult Index()
         {
